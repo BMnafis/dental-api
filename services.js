95c79d9dc7d7a8996969cb9531c01fe58a3dc873
BMnafis <mdnafis215@gmail.com>
2021-10-18 20:05:24 +0600

    first commit

---
+++ services.js
@@ -0,0 +1,79 @@
+
+const services = [
+    {
+        id: 1,
+        title: 'Braces and Orthodontics',
+        image: 'https://i.ibb.co/yRTNpRW/braces.jpg',
+        description: 'We use Dental Braces in Orthodontics to align and straighten teeth and help position them with regard to a persons bite.',
+        megaDescription: `The purpose of orthodontic treatment is to make the best of your teeth.
+
+        This includes straightening your teeth so you're able to care for your teeth and gums more easily, and improving your bite so you can eat more comfortably. And your smile will benefit, too.
+        
+        Treatment almost always involves using braces to straighten crooked, crowded or protruding teeth, close gaps between teeth, and correct the bite so the top and bottom teeth meet when the mouth is closed.
+        
+        You'll need to have healthy teeth and gums before you can have a brace fitted.
+        
+        This is because you must be able to keep your teeth and your brace very clean while you're wearing it to avoid getting tooth decay or gum disease.
+        
+        Treatment usually lasts from 18 months to 2 years, and visits to the orthodontist are needed every 6 to 8 weeks.
+        
+        Orthodontic treatment is available on the NHS for young people under the age of 18 at no cost.
+        
+        NHS orthodontic treatment isn't usually available for adults, but may be approved on a case-by-case basis if needed for health reasons.
+        
+        Your dentist can give you more information.`
+    },
+    {
+        id: 2,
+        title: 'Cosmetic Dentistry',
+        image: 'https://i.ibb.co/ZmkMsd9/cosmetic.jpg',
+        description: 'Cosmetic dentistry is dentistry aimed at creating a positive change to your teeth and to your smile.',
+        megaDescription: `Teeth whitening can be one of the simplest and least expensive ways to improve your smile. Teeth can be bleached with in-office products in your dentist’s office for about $500, or you can buy a mold and gels from your dentist to bleach your teeth at home. There are also whitening products available over the counter at retail stores for convenient at-home whitening: whitening toothpastes, rinses, and whitestrips. These products together run about $3 - $50.
+
+
+        Dental veneers are wafer-thin, custom-made shells of tooth-colored porcelain or resin that cover the front surface of the teeth. After removing about a half-millimeter of enamel from the tooth surface, these thin shells are bonded (cemented) to the front of the teeth, changing their color, shape, size, or length. Veneers are often called “Hollywood teeth." Living up to that name, this process can cost up to $500-$1,300 per tooth.
+        `
+    },
+    {
+        id: 3,
+        title: 'General Dentistry',
+        image: 'https://i.ibb.co/8jHg9N0/general.jpg',
+        description: 'A general dentist is your primary care dental provider. This dentist diagnoses, treats, and manages your overall oral health care needs.',
+        megaDescription: `Many different types of oral health care providers could become involved in the care of your teeth, gums, and mouth. A brief description of these various health care providers is as follows:
+
+        A general dentist is your primary care dental provider. This dentist diagnoses, treats, and manages your overall oral health care needs, including gum care, root canals, fillings, crowns, veneers, bridges, and preventive education.
+        
+        All practicing general dentists have earned either a DDS or DMD degree (doctor of dental surgery or doctor of dental medicine, respectively). There is no difference between the two degrees or the curriculum requirements that dentists must meet. Some schools simply award the one degree, while others award the other.
+        `
+    },
+    {
+        id: 4,
+        title: 'Dental Implants Surgery',
+        image: 'https://i.ibb.co/wQjPm1D/implant.jpg',
+        description: 'Procedure that replaces tooth roots with metal, screwlike posts and replaces damaged or missing teeth with artificial teeth that look and function much like real ones.',
+        megaDescription: `Dental implant surgery is a procedure that replaces tooth roots with metal, screwlike posts and replaces damaged or missing teeth with artificial teeth that look and function much like real ones. Dental implant surgery can offer a welcome alternative to dentures or bridgework that doesn't fit well and can offer an option when a lack of natural teeth roots don't allow building denture or bridgework tooth replacements.
+
+        How dental implant surgery is performed depends on the type of implant and the condition of your jawbone. Dental implant surgery may involve several procedures. The major benefit of implants is solid support for your new teeth — a process that requires the bone to heal tightly around the implant. Because this bone healing requires time, the process can take many months.        
+        `
+    },
+    {
+        id: 5,
+        title: 'Oral Surgery',
+        image: 'https://i.ibb.co/sHq5pRf/oral.jpg',
+        description: 'We have surgical procedure performed in or around your mouth and jaw, usually by a dental specialist who is trained to perform certain kinds of oral surgeries.',
+        megaDescription: `Oral surgery refers to any surgical procedure performed in or around your mouth and jaw, usually by a dental specialist who’s trained to perform certain kinds of oral surgeries.
+
+        There are many types of oral surgery, including surgical procedures such as a tooth removal or a root canal. Some oral surgeries involve the repositioning of the jaw. In other instances, oral surgery may involve the removal of a tumor. Oral surgeries might be performed by any one of a number of different kinds of dental specialists, including endodontists, periodontists, and prosthodontists.               
+        `
+    },
+    {
+        id: 6,
+        title: 'Oral Surgery',
+        image: 'https://i.ibb.co/wL9MzTr/Teledentistry.jpg',
+        description: 'Teledentistry uses technology to provide patients with dental care without visiting a physical office.',
+        megaDescription: `When you think of your daily work as a dental hygienist, you probably envision yourself moving from patient to patient, scaling teeth, assessing disease, exposing radiographs, educating on oral health, and otherwise preparing patients for a doctor exam. But, there is a new way dentists are conducting business. It’s called Teledentistry. Think of teledentistry as a high-tech way for patients to get the care they need from professionals in the field of dentistry. Discover exactly what teledentistry is and how you fit into this new technology as a dental hygienist.
+
+        Teledentistry uses technology to provide patients with dental care without visiting a physical office. A patient takes a photo or video of a sore or aching tooth and shares it with a dentist from a remote location. The dentist examines the video or photo to determine the problem with the tooth. Next, he or she prescribes medication to help with the discomfort caused by the tooth until it heals or until the person can visit the physical office for a closer examination. That is just one example of how teledentistry is used. In this scenario, a dental hygienist would help gather information from the patient, so the dentist has as much data as he or she needs to make a diagnosis.               
+        `
+    }
+]
